95216ab7f69242a69e15c4512c723f47ac90a468
Adriel <1750927@uab.cat>
2025-02-23 09:20:06 +0100

    Added Random Small_Graph Generator (solved trivial bugs)

--- practica_1.py
+++ practica_1.py
@@ -2,21 +2,18 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-# Define number of nodes and edges
-num_nodes = 10
-num_edges = 15
+def create_simple_graph (num_nodes=10, num_edges=15):
+    # Create an empty graph
+    G = nx.Graph()
+    # Add nodes
+    G.add_nodes_from(range(num_nodes))
 
-# Create an empty graph
-G = nx.Graph()
-
-# Add nodes
-G.add_nodes_from(range(num_nodes))
-
-edges = set()
-while len(edges) < num_edges:
-    u, v = random.sample(range(num_nodes), 2)  # Pick two distinct nodes
-    edges.add((u, v))
-G.add_edges_from(edges)
-
-nx.draw(G, with_labels=True)
+    edges = set()
+    while len(edges) < num_edges:
+        u, v = random.sample(range(num_nodes), 2)  # Pick two distinct nodes
+        edges.add((u, v))
+    G.add_edges_from(edges)
+    return G
+my_graph = create_simple_graph()
+nx.draw(my_graph, with_labels=True)
 plt.show()
