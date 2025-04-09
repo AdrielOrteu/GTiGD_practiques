0b6bcd04d6f9e66221845c321f72ccfde52d52a8
adrie <1750927@uab.cat>
2025-04-09 18:51:57 +0200

    LEARNING HOW greedy_color() WORKS

--- practica_2.py
+++ practica_2.py
@@ -20,34 +20,19 @@ def task_2():
 
 def task_3():
     import matplotlib.colors as mpl
+    import matplotlib.pyplot as plt
+    
     G = create_simple_graph()
     # Apply greedy coloring
     show_graph(G)
-    graph_coloring = nx.greedy_color(G)
-    print(f"G (nodes) -> {G.nodes}")
-    print(f"graph_coloring -> {graph_coloring}")
-    for k in graph_coloring:
-        print(k)
-    print(f"graph_coloring.values() -> {graph_coloring.values()}")
+    graph_coloring = nx.greedy_color(G) # creates a dictionary where the keys are the nodes and the values are the colors
     unique_colors = set(graph_coloring.values())
-    print(f"unique_colors -> {unique_colors}")
     # Assign colors to nodes based on the greedy coloring
-    graph_color_to_mpl_color = dict(zip(unique_colors, mpl.TABLEAU_COLORS))
-    print(f"graph_color_to_mpl_color -> {graph_color_to_mpl_color}")
+    graph_color_to_mpl_color = dict(zip(unique_colors, mpl.TABLEAU_COLORS)) # creates a dictionary where the keys are the colors and the values are the color names (for MatPlotLib to understand)
     node_colors = [graph_color_to_mpl_color[graph_coloring[n]] for n in G.nodes()]
+    print(node_colors)
     
     pos = nx.spring_layout(G, seed=14)
-    nx.draw(
-        G,
-        pos,
-        with_labels=True,
-        node_size=500,
-        node_color=node_colors,
-        edge_color="grey",
-        font_size=12,
-        font_color="#333333",
-        width=2,
-    )
-    
-    show_graph(G)
+    nx.draw(G, pos, with_labels=True, node_color=node_colors)
+    plt.show()
 task_3()
