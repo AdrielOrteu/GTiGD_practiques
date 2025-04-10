b671fe353930e9d8bc8dd476e8e8a1c2e59d07c8
adrie <1750927@uab.cat>
2025-04-10 11:21:49 +0200

    Adriel - Changes to compleat task_3

--- practica_2.py
+++ practica_2.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import random
 import typing
-from grafSimple import project_graph_create, create_simple_graph, show_graph
+from grafSimple import *
 
 
 def task_1():
@@ -22,17 +22,36 @@ def task_3():
     import matplotlib.colors as mpl
     import matplotlib.pyplot as plt
     
-    G = create_simple_graph()
-    # Apply greedy coloring
-    show_graph(G)
-    graph_coloring = nx.greedy_color(G) # creates a dictionary where the keys are the nodes and the values are the colors
-    unique_colors = set(graph_coloring.values())
-    # Assign colors to nodes based on the greedy coloring
-    graph_color_to_mpl_color = dict(zip(unique_colors, mpl.TABLEAU_COLORS)) # creates a dictionary where the keys are the colors and the values are the color names (for MatPlotLib to understand)
-    node_colors = [graph_color_to_mpl_color[graph_coloring[n]] for n in G.nodes()]
-    print(node_colors)
+    @timer
+    def greedy_coloring(graph):
+        # Apply greedy coloring
+        graph_coloring = nx.greedy_color(graph) # creates a dictionary where the keys are the nodes and the values are the colors
+        print(graph_coloring)
+        unique_colors = set(graph_coloring.values())
+        return graph, graph_coloring, unique_colors
     
-    pos = nx.spring_layout(G, seed=14)
-    nx.draw(G, pos, with_labels=True, node_color=node_colors)
-    plt.show()
+    
+    
+    def show_colored_graph(graph):
+        # Assign colors to nodes based on the greedy coloring
+        graph_color_to_mpl_color = dict(zip(unique_colors, plt.cm.get_cmap("tab20").colors))  # creates a dictionary where the keys are the colors and the values are the tableau-color names (intermediate step for MatPlotLib to understand)
+        node_colors = [graph_color_to_mpl_color[graph_coloring[n]] for n in G.nodes()]
+        pos = nx.spring_layout(graph, seed=14)
+        nx.draw(graph, pos, with_labels=True, node_color=node_colors)
+        plt.show()
+    
+    
+    test_list = [ [ (i**2, j**2) for j in range(2, i**2)] for i in range(10, 20)]
+    print(test_list)
+    for test_group in test_list:
+        for test in test_group:
+            print("\n###_NEW-TEST_###")
+            G = create_simple_graph(test[0], test[1])
+            print(test)
+            # show_graph(G)
+            
+            G, graph_coloring, unique_colors = greedy_coloring(G)
+            print(f"num of colors needed: {len(unique_colors)}\ncolors used dictionary: {graph_coloring}")
+            # show_colored_graph(G)
+    
 task_3()
