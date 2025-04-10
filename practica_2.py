b459ae33c32a540334493dd90b4acee3a8d3d743
saul <saulmarco06@gmail.com>
2025-04-10 11:22:51 +0200

    commit guardar cambios saul

--- practica_2.py
+++ practica_2.py
@@ -1,6 +1,17 @@
 import networkx
 import random
 import typing
+import matplotlib
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time()
+        print(f"Execution time: {t2 - t1:.6f} seconds")
+        return result
+    return wrapper
 
 
 def task_1():
@@ -14,11 +25,28 @@ def task_1():
 
 
 def task_2():
-    n = input("Introduce n: ")
-    m = input("Introduce m: ")
-    combinacion = intput("Introduce el código:")
+    n = int(input("Introduce cantidad de números a elegir (n): "))
+    m = int(input("Introduce cantidad de números posibles (m): "))
+
+    combinacion = input("Introduce el código: ")
+    combinacion = list(map(int, combinacion.split()))
+    combinacion.sort()
+
+    intentos = 0
+    ganador = []
+
+    while combinacion != ganador:
+        ganador = random.sample(range(1, m + 1), n)
+        ganador.sort()
+        intentos += 1
+
+    print("Has ganado tras {intentos} intentos.")
+
+
+
 
 
 
 def task_3():
     pass
+task_2()
